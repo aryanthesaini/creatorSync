0b463ccf92a00fda390457b5573b7778e208d4e3
Aryan Saini <aryan@Aryans-MacBook-Air.local>
2024-02-15 16:32:02 +0530

    first commit -2

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
     <section class='bg-gray-900 text-white'>
       <div class='mx-auto max-w-screen-xl px-4 py-32 lg:flex h-screen items-center'>
         <div class='mx-auto max-w-3xl text-center'>
-          <h1 class='bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-7xl font-extrabold text-transparent '>
+          <h1 class='bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-5xl md:text-7xl font-extrabold text-transparent '>
             CreatorSync
             <span class='block text-xl pt-11 font-medium text-white'>
               {' '}
